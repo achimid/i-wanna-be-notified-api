35dd0c520049f5178af73c6cf1ab472efd74e512
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-07-25 20:36:25 -0300

    adicionando monitoringId

--- src/execution/execution-service.js
+++ src/execution/execution-service.js
@@ -3,8 +3,8 @@ const producer = require('./execution-producer')
 const executionModel = require('./execution-model')
 
 const findByFilter = (filter) => {
-    const { uuid, url, isSuccess, level, hashTarget } =  filter
-    filter = clearObj({ uuid, url, isSuccess, level, hashTarget })
+    const { uuid, url, isSuccess, level, hashTarget, monitoringId } =  filter
+    filter = clearObj({ uuid, url, isSuccess, level, hashTarget, monitoringId })
 
     return executionModel.find(filter).lean()
 }
