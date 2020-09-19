d995b7c84eff63cf1c1847d58b66230b3f9488c5
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-09-19 11:05:11 -0300

    Removendo o start do db velho

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const express = require('express')
 const compression = require('compression')
 
 const routes = require('./config/routes')
-const { databaseInit } = require('./config/database')
 
 const app = express()
 
@@ -18,6 +17,5 @@ app.disable('x-powered-by')
 
 // Initializations
 routes(app)
-    .then(databaseInit)
 
 app.listen(process.env.PORT)
