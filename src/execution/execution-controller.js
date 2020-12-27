6776bd11e8ec95e5fd2d7df1b0859271da5c9497
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-12-27 15:47:59 -0300

    Removendo endpoint de teste

--- src/execution/execution-controller.js
+++ src/execution/execution-controller.js
@@ -17,11 +17,4 @@ router.get('/:id', (req, res) => {
         .then(execution => res.status(OK).send(execution))
 })
 
-router.post('/test', (req, res) => {
-    console.log('Create a new execution on queue process')
-    return service
-        .postExecution(req.body)
-        .then(execution => res.status(CREATED).send(execution))
-})
-
 module.exports = router
