2585bd172fbf0235fb7b5b229748845ccb880f9d
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-12-27 13:28:26 -0300

    Adicionando timeout default da requisição e aumentando a capacidade do tamanho da request

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('appmetrics-dash').attach()
 const cors = require('cors')
 const express = require('express')
 const compression = require('compression')
+const bodyParser = require('body-parser')
 
 const routes = require('./config/routes')
 
@@ -12,10 +13,12 @@ const app = express()
 
 app.use(cors())
 app.use(compression())
-app.use(express.json())
+app.use(bodyParser.json({limit: '20mb'}))
 app.disable('x-powered-by')
 
 // Initializations
 routes(app)
 
-app.listen(process.env.PORT)
+app
+    .listen(process.env.PORT)
+    .setTimeout(parseInt(process.env.DEFAULT_TIMEOUT_REQUEST) * 1000 * 60)
