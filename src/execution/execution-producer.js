c529269df49843b8cccf1ab5e5d7c9ee88ce256c
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2021-01-15 16:46:59 -0300

    Refatorando service e finalizando crawler

--- src/execution/execution-producer.js
+++ src/execution/execution-producer.js
@@ -1,7 +1,7 @@
 const queue = require("../utils/queue")
 
 
-const postExecution = (data) => queue.sendToQueue("EXECUTION_INCOMING", data)
+const postExecution = (data) => queue.sendToQueue("INCOMING", data)
 
 module.exports = {
     postExecution
